79b20019bdfe030e8c01e640b2fb6b2a9108325d
Johnpaul Eze <ezejohnpaul846@gmail.com>
2020-08-03 22:20:56 +0100

    my first commit

---
+++ src/TodoItems.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import FlipMove from "react-flip-move";
+
+class TodoItems extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.createTasks = this.createTasks.bind(this);
+    }
+
+    createTasks(item) {
+    return <li onClick={() => this.delete(item.key)}
+                key={item.key}>{item.text}</li>
+    }
+
+    delete(key) {
+        console.log("key is: " + key);
+        this.props.delete(key);
+    }
+
+    render() {
+        var todoEntries = this.props.entries;
+        var listItems = todoEntries.map(this.createTasks);
+    
+        return ( 
+            <div>
+                <ul className="theList">
+                    <FlipMove duration={150} easing="ease-out">
+                         {listItems}
+                    </FlipMove>
+                </ul>
+            </div>
+         );
+    }
+}
+ 
+export default TodoItems;
